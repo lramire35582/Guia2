75c400b412bfe3ca5fb9ddc0febcb7fea82dd07d
lramire35582 <lramire35582@universidadean.edu.co>
2017-08-13 15:45:41 -0500

    Intercambio inicial de proyecto

---
+++ Estudiante.java
@@ -0,0 +1,23 @@
+
+
+public class Estudiante extends Persona
+{
+    public int carrera(){
+        return 1; 
+    }
+    public int matricula(){
+        return 2; 
+    }
+    
+    static String estudiarMedtod()
+    {
+        String devul = "estudiar";
+        return devul;
+    }
+    
+    static String hacerexamenMedtod()
+    {
+        String devul = "Hacer examen";
+        return devul;
+    }
+}
---
+++ Persona.java
@@ -0,0 +1,22 @@
+
+public class Persona
+{
+    public int nombre(){
+        return 1; 
+    }
+    public int edad(){
+        return 2; 
+    }
+    
+    static String dormirMedtod()
+    {
+        String devul = "dormir";
+        return devul;
+    }
+    
+    static String comerMedtod()
+    {
+        String devul = "comer";
+        return devul;
+    }
+}
